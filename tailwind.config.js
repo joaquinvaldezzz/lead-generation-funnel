dca4b32786a678206687cd918c57409f6484a239
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-01-04 13:26:44 +0800

    Extend color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,32 @@ module.exports = {
   content: ['./src/**/*.astro'],
   theme: {
     extend: {
+      colors: {
+        'green-vogue': {
+          50: '#e5f9ff',
+          100: '#d0f3ff',
+          200: '#aae8ff',
+          300: '#77d4ff',
+          400: '#42afff',
+          500: '#1887ff',
+          600: '#006fff',
+          700: '#0072ff',
+          800: '#0064e0',
+          900: '#012250', // Original color
+        },
+        'green-yellow': {
+          50: '#f9ffe5',
+          100: '#efffc7',
+          200: '#dfff96',
+          300: '#c0fd45', // Original color
+          400: '#adf427',
+          500: '#8eda08',
+          600: '#6daf01',
+          700: '#528506',
+          800: '#43680c',
+          900: '#39580f',
+        },
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
